14dc2f9bab0a7ad567022e3b8a662cbac7ec1226
rafael <rafaelbaugar@gmail.com>
2025-05-14 03:31:59 -0600

    almost done

--- assets/main.js
+++ assets/main.js
@@ -9,8 +9,11 @@ window.onload = async function () {
     pokemon.id = i;
     pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
     pokemon.classList.add("pokemon-name");
+    pokemon.addEventListener("click", updatePokemon);
     document.querySelector(".pokemon-list").append(pokemon);
   }
+
+  document.querySelector(".pokemon-description").innerText = pokedex[1]["desc"];
 };
 
 async function getPokemon(num) {
@@ -33,4 +36,25 @@ async function getPokemon(num) {
     types: pokemonType,
     desc: pokemonDesc,
   };
+}
+
+function updatePokemon() {
+  document.querySelector(".pokemon-img").src = pokedex[this.id]["img"];
+
+  let typesDiv = document.querySelector(".pokemon-types");
+  while (typesDiv.firstChild) {
+    typesDiv.firstChild.remove();
+  }
+
+  let types = pokedex[this.id]["types"];
+  for (let i = 0; i < types.length; i++) {
+    let type = document.createElement("span");
+    type.innerText = types[i]["type"]["name"].toUpperCase();
+    type.classList.add("type-box");
+    type.classList.add(types[i]["type"]["name"]);
+    typesDiv.append(type);
+  }
+
+  document.querySelector(".pokemon-description").innerText =
+    pokedex[this.id]["desc"];
 }
